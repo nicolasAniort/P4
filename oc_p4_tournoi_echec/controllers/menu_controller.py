ef0fe6fc66d45aab4d8714735ee4ad95d95c2ccc
nicolasAniort <nicolas.aniort@gmail.com>
2023-03-07 18:31:17 +0100

    Boucles WHILE pour conserver contextes des menus

--- oc_p4_tournoi_echec/controllers/menu_controller.py
+++ oc_p4_tournoi_echec/controllers/menu_controller.py
@@ -23,101 +23,112 @@ class MenuController():
             self.main_menu()
 
     def main_menu(self):
-
-        self.menu.display_main_menu()
-        nb_choice = self.menu.choice()
-           
-        match str(nb_choice):
-            case "1": 
-                """acces au sous menu joueur"""
-                self.menu.display_players_menu()
-                nb_choice = self.menu.choice()
-                match str(nb_choice):
-                    case "11": 
-                        self.playercontroller.write_player()
-                        self.main_menu()
-                                              
-                    case "12": 
-                        self.playercontroller.change_rank_player()
-                        self.main_menu()  
-                                             
-                    case "13": 
-                        self.main_menu()
-                    
-            case "2":
-                """acces au sous menu tournois""" 
-                self.menu.display_tournaments_menu()
-                nb_choice = self.menu.choice()
-                match str(nb_choice):
-                    case "21": 
-                        # crééer un tournoi
-                        print("retour dans menu controller apres creation du joueur")
-                        self.tournamentcontroller.create_tournament()
-                        self.main_menu()
-                                              
-                    case "22": 
-                        #Selectionner un tournoi
-                        tournament_choice = self.tournamentcontroller.list_tournament_for_choice()
-                        print("***")
-                        print(tournament_choice)
-                        print("***")
-                        print("tournament choisi :", tournament_choice['nom_du_tournoi'])
-                        #acces au sous menu tournois 
-                        self.menu.display_tournaments_submenu(tournament_choice)                        
-                        nb_choice = self.menu.choice()        
+        x = 0
+        while x < 1:
+            self.menu.display_main_menu()
+            nb_choice = self.menu.choice()
+            
+            match str(nb_choice):
+                case "1": 
+                    """acces au sous menu joueur"""
+                    y = 0
+                    while y < 1:
+                        self.menu.display_players_menu()
+                        nb_choice = self.menu.choice()
                         match str(nb_choice):
-                            case "221":
-                                #Ajouter un joueur au tournoi
-                                self.tournamentcontroller.add_player_to_tournament(tournament_choice)
+                            case "11": 
+                                self.playercontroller.write_player()
+                                y = 0
+                                #self.main_menu()
+                                                    
+                            case "12": 
+                                self.playercontroller.change_rank_player()
+                                #self.menu.display_players_menu()
+                                y = 0
+                                #self.main_menu()  
+                                                    
+                            case "13": 
+                                #self.main_menu()
+                                y = 2
+                case "2":
+                    """acces au sous menu tournois"""
+                    z = 0
+                    while z < 1:
+                        self.menu.display_tournaments_menu()
+                        nb_choice = self.menu.choice()
+                        match str(nb_choice):
+                            case "21": 
+                                # crééer un tournoi
+                                print("retour dans menu controller apres creation du joueur")
+                                #self.tournamentcontroller.create_tournament()
+                                z = 0
+                                                    
+                            case "22": 
+                                #Selectionner un tournoi
+                                tournament_choice = self.tournamentcontroller.list_tournament_for_choice()
+                                print("***")
+                                print(tournament_choice)
+                                print("***")
+                                print("tournament choisi :", tournament_choice['nom_du_tournoi'])
+                                #acces au sous menu tournois 
+                                self.menu.display_tournaments_submenu(tournament_choice)                        
+                                nb_choice = self.menu.choice()        
+                                match str(nb_choice):
+                                    case "221":
+                                        #Ajouter un joueur au tournoi
+                                        self.tournamentcontroller.add_player_to_tournament(tournament_choice)
+                                        
+                                    case "222":
+                                        #Générer les tours et les matchs et afficher le tableau 
+                                        pass                            
+                                    case "223":
+                                        #Saisie des scores
+                                        pass
+                                    case "224":
+                                        #retour au menu supérieur
+                                        pass
+                                z = 0
+                                #print("retour dans menu controller apres modification du classement du joueur")
                                 
-                            case "222":
-                                #Générer les tours et les matchs et afficher le tableau 
-                                pass                            
-                            case "223":
-                                #Saisie des scores
+                            case "23": 
+                                z = 1
+                            
+                case "3": 
+                    """acces au sous menu rapport"""
+                    w = 0
+                    while w < 1:
+                        self.menu.display_rapport_menu()
+                        nb_choice = self.menu.choice()
+                        match str(nb_choice):
+                            case "31": 
+                                # affichage des joueurs par ordre alphabetique
+                                new_report = PlayerController()
+                                new_report.list_registred_players()
+                                w = 0
+                                                    
+                            case "32": 
+                                # affichage des joueurs par score
+                                new_repport_rank_player = PlayerController()
+                                new_repport_rank_player.list_players_by_rank()
+                                w = 0
+                                
+                            case "33": 
+                                # affichage de tous les tournois
+                                new_report_tournament = TournamentController()
+                                new_report_tournament.list_registred_tournaments() 
+                                w = 0
+                                
+                            case "34": 
+                                # affichage des joueurs par ordre alphabetique et par tours
                                 pass
-                            case "224":
-                                #retour au menu supérieur
+                            case "35": 
+                                # affichage des joueurs par score et par tours
                                 pass
-                        print("retour dans menu controller apres modification du classement du joueur")
-                        
-                    case "23": 
-                        self.main_menu()
-                    
-            case "3": 
-                """acces au sous menu rapport"""
-                self.menu.display_rapport_menu()
-                nb_choice = self.menu.choice()
-                match str(nb_choice):
-                    case "31": 
-                        # affichage des joueurs par ordre alphabetique
-                        new_report = PlayerController()
-                        new_report.list_registred_players()
-                        self.main_menu()
-                                              
-                    case "32": 
-                        # affichage des joueurs par score
-                        new_repport_rank_player = PlayerController()
-                        new_repport_rank_player.list_players_by_rank()
-                        self.main_menu()
-                        
-                    case "33": 
-                        # affichage de tous les tournois
-                        new_report_tournament = TournamentController()
-                        new_report_tournament.list_registred_tournaments() 
-                        self.main_menu()
-                        
-                    case "34": 
-                        # affichage des joueurs par ordre alphabetique et par tours
-                        pass
-                    case "35": 
-                        # affichage des joueurs par score et par tours
-                        pass
-                    case "36": 
-                        self.main_menu()
+                            case "36": 
+                                w = 1
 
-            case "4": 
-                self.confirm_quit()
-  
+                case "4": 
+                    self.confirm_quit()
+    
     
     
